9d2750a7e4fb6e2496a0cd329865d5da6f6cbd87
pranavanexusgroup-ux <pranavanexusgroup@gmail.com>
2026-09-20 16:11:33 +0530

    Configure Vite for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/version-1/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
